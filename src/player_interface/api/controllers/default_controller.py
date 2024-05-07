ccbde3066a8b4408a985d8abeb980b6ebe7f1fec
zfoteff <zfoteff@protonmail.com>
2024-05-06 23:10:28 -0700

    player interface styling for landing page

--- src/player_interface/api/controllers/default_controller.py
+++ src/player_interface/api/controllers/default_controller.py
@@ -10,6 +10,7 @@ templates = Jinja2Templates(directory="api/templates")
 
 class DefaultController:
     async def render_homepage(request: Request) -> HTMLResponse:
+        
         return templates.TemplateResponse("home.html", context={"request": request})
 
     async def render_stats_page(request: Request) -> HTMLResponse:
