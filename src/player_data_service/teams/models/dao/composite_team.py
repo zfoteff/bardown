e8954ce8b72231918969b0d281a38919302f718e
zfoteff <zfoteff@zagmail.gonzaga.edu>
2025-09-10 17:23:04 -0700

    work

--- src/player_data_service/teams/models/dao/composite_team.py
+++ src/player_data_service/teams/models/dao/composite_team.py
@@ -1,5 +1,5 @@
 from teams.models.dao.team import Team
-from typing import List
+from typing import List, Tuple, Dict
 
 
 class CompositeTeamPlayer:
@@ -14,6 +14,48 @@ class CompositeTeamPlayer:
     school: str
     img_url: str
 
+    def __init__(
+        self,
+        team_id: str = None,
+        year: int = None,
+        player_id: str = None,
+        first_name: str = None,
+        last_name: str = None,
+        number: int = None,
+        position: str = None,
+        grade: str = None,
+        school: str = None,
+        img_url: str = None,
+    ):
+        self.team_id = team_id
+        self.year = year
+        self.player_id = player_id
+        self.first_name = first_name
+        self.last_name = last_name
+        self.number = number
+        self.position = position
+        self.grade = grade
+        self.school = school
+        self.img_url = img_url
+
+    @classmethod
+    def from_tuple(cls, composite_team_coach_tuple: Tuple) -> None:
+        return cls(**{k: v for k, v in zip(cls().to_dict().keys(), composite_team_coach_tuple)})
+
+    def to_dict(self) -> Dict:
+        return {
+            "team_id": f"{self.team_id}",
+            "year": self.year,
+            "player_id": f"{self.player_id}",
+            "first_name": f"{self.first_name}",
+            "last_name": f"{self.last_name}",
+            "number": f"{self.number}",
+            "position": f"{self.position}",
+            "grade": f"{self.grade}",
+            "school": f"{self.school}",
+            "img_url": f"{self.img_url}",
+        }
+
 
 class CompositeTeamCoach:
     team_id: str
@@ -26,12 +68,50 @@ class CompositeTeamCoach:
     img_url: str
 
     def __init__(
-        self, team_id: str = None, year: int = None, coach_id: str = None, first_name: str = None
+        self,
+        team_id: str = None,
+        year: int = None,
+        coach_id: str = None,
+        first_name: str = None,
+        last_name: str = None,
+        role: str = None,
+        since: int = None,
+        img_url: str = None,
     ):
-        pass
+        self.team_id = team_id
+        self.year = year
+        self.coach_id = coach_id
+        self.first_name = first_name
+        self.last_name = last_name
+        self.role = role
+        self.since = since
+        self.img_url = img_url
+
+    @classmethod
+    def from_tuple(cls, composite_team_coach_tuple: Tuple) -> None:
+        return cls(**{k: v for k, v in zip(cls().to_dict().keys(), composite_team_coach_tuple)})
+
+    def to_dict(self) -> Dict:
+        return {
+            "team_id": f"{self.team_id}",
+            "year": self.year,
+            "coach_id": f"{self.coach_id}",
+            "first_name": f"{self.first_name}",
+            "last_name": f"{self.last_name}",
+            "role": f"{self.role}",
+            "since": f"{self.since}",
+            "img_url": f"{self.img_url}",
+        }
 
 
 class CompositeTeam:
     team: Team
     players: List[CompositeTeamPlayer]
     coaches: List[CompositeTeamCoach]
+
+    def __init__(
+        self, team: Team, players: List[CompositeTeamPlayer], coaches: List[CompositeTeamCoach]
+    ):
+        self.team = team
+        self.players = players
+        self.coaches = coaches
